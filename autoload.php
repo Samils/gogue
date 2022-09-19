27f023ee0c8dfbe77cddb0e6991205ed9958dc8f
Samil <agostinhosaml832@gmail.com>
2022-09-19 13:33:54 +0100

    root: create pack autoload files

---
+++ autoload.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @version 2.0
+ * @author Sammy
+ *
+ * @keywords Samils, ils, php framework
+ * -----------------
+ * @namespace Sammy\Packs\Gogue
+ * - Autoload, application dependencies
+ */
+namespace Sammy\Packs\Gogue {
+  $autoloadFile = __DIR__ . '/vendor/autoload.php';
+
+  if (is_file ($autoloadFile)) {
+    include_once $autoloadFile;
+  }
+
+  $initFileListDirPath = join (DIRECTORY_SEPARATOR, [
+    __DIR__, 'src', 'cli', 'commands'
+  ]);
+
+  Helper::AutoloadInitFileDir ($initFileListDirPath);
+}
